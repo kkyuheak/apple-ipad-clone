f0f760c02e83ba314fe37d159271c332b8c0222b
kkyuheak <gyuheak@gmail.com>
2023-04-27 00:40:19 +0900

    헤더 검색 js 적용중

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// 장바구니
+
 const basketStarterEl = document.querySelector(".basket-starter");
 const basketEl = basketStarterEl.querySelector(".basket");
 
@@ -25,3 +27,25 @@ basketEl.addEventListener("click", (e) => {
 window.addEventListener("click", () => {
   hideBasket();
 });
+
+// 검색
+
+const headerEl = document.querySelector("header");
+const searchWrapEl = headerEl.querySelector(".search-wrap");
+const searchStarterEl = headerEl.querySelector(".search-starter");
+const searchCloserEl = searchWrapEl.querySelector(".search-closer");
+const searchShadowEl = searchWrapEl.querySelector(".shadow");
+
+const showSearch = () => {
+  headerEl.classList.add("searching");
+  document.documentElement.classList.add("fixed");
+};
+
+const hideSearch = () => {
+  headerEl.classList.remove("searching");
+  document.documentElement.classList.remove("fixed");
+};
+
+searchStarterEl.addEventListener("click", showSearch);
+searchCloserEl.addEventListener("click", hideSearch);
+searchShadowEl.addEventListener("click", hideSearch);
