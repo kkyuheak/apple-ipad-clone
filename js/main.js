5164712a48f98ba3f5a9b3dec0ce9df1521dd2fb
kkyuheak <gyuheak@gmail.com>
2023-04-27 23:33:11 +0900

    헤더 검색 애니메이션 적용

--- js/main.js
+++ js/main.js
@@ -31,19 +31,34 @@ window.addEventListener("click", () => {
 // 검색
 
 const headerEl = document.querySelector("header");
+const headerMenuEls = [...document.querySelectorAll(".menu > li")];
 const searchWrapEl = headerEl.querySelector(".search-wrap");
 const searchStarterEl = headerEl.querySelector(".search-starter");
 const searchCloserEl = searchWrapEl.querySelector(".search-closer");
 const searchShadowEl = searchWrapEl.querySelector(".shadow");
+const searchDelayEl = [...searchWrapEl.querySelectorAll("li")];
 
 const showSearch = () => {
   headerEl.classList.add("searching");
   document.documentElement.classList.add("fixed");
+  headerMenuEls.reverse().forEach((el, index) => {
+    el.style.transitionDelay = (index * 0.4) / headerMenuEls.length + "s";
+  });
+  searchDelayEl.forEach((el, index) => {
+    el.style.transitionDelay = (index * 0.4) / searchDelayEl.length + "s";
+  });
 };
 
 const hideSearch = () => {
   headerEl.classList.remove("searching");
   document.documentElement.classList.remove("fixed");
+  headerMenuEls.reverse().forEach((el, index) => {
+    el.style.transitionDelay = (index * 0.4) / headerMenuEls.length + "s";
+  });
+  searchDelayEl.reverse().forEach((el, index) => {
+    el.style.transitionDelay = (index * 0.4) / searchDelayEl.length + "s";
+  });
+  searchDelayEl.reverse();
 };
 
 searchStarterEl.addEventListener("click", showSearch);
