aede99905dcfa6c954d0b34bb385e99db4af72da
kkyuheak <gyuheak@gmail.com>
2023-05-17 23:56:00 +0900

    footer 작업중

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-// 장바구니
+import ipads from "../data/ipads.js";
 
+// 장바구니
 const basketStarterEl = document.querySelector(".basket-starter");
 const basketEl = basketStarterEl.querySelector(".basket");
 
@@ -101,4 +102,32 @@ pauseBtn.addEventListener("click", () => {
   video.pause();
   playBtn.classList.remove("hide");
   pauseBtn.classList.add("hide");
+});
+
+// 당신에게 맞는 아이패드는 랜더링
+const itemsEl = document.querySelector(".compare .items");
+ipads.forEach((ipad) => {
+  const itemEL = document.createElement("div");
+  itemEL.classList.add("item");
+
+  let colorList = "";
+
+  ipad.colors.forEach((color) => {
+    colorList += `<li style="background-color:${color};"></li>`;
+  });
+  itemEL.innerHTML = /* html */ `
+    <div class="thumbnail">
+      <img src="${ipad.thumbnail}" alt="${ipad.name}">
+    </div>
+    <ul class="colors">
+      ${colorList}
+    </ul>
+    <h3 class="name">${ipad.name}</h3>
+    <p class="tagline">${ipad.tagline}</p>
+    <p class="price">₩${ipad.price.toLocaleString("en-US")}부터</p>
+    <button class="btn">구입하기</button>
+    <a href="${ipad.url}" class="link">더 알아보기</a>
+  `;
+
+  itemsEl.append(itemEL);
 });
